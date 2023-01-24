d09e21ac72f2747c85209e29a11c64463c7633ed
codehass <hassan.elouardy06@gmail.com>
2023-01-24 11:30:59 +0100

    Store configuration

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,10 +1,10 @@
-import { combineReducers } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
 import booksReducer from './books/books';
 import categoriesReducer from './categories/categories';
 
-const allReducers = combineReducers({
-  booksReducer,
-  categoriesReducer,
+export default configureStore({
+  reducer: {
+    booksReducer,
+    categoriesReducer,
+  },
 });
-
-export default allReducers;
